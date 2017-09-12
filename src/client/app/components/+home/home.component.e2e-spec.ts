a2fc2ecb4414e1d8f6ab07be8b5b33b1cace95f6
Burak Tasci <burak.tasci@cozumtech.net>
2017-09-12 20:18:54 +0300

    test(e2e): add basic e2e tests

---
+++ src/client/app/components/+home/home.component.e2e-spec.ts
@@ -0,0 +1,19 @@
+// testing
+import { baseUrl, browser } from 'e2e-config';
+import { e2e } from '../../framework/testing/e2e';
+
+e2e.describe('ng-seed/universal', () => {
+  e2e.describe('components', () => {
+    e2e.describe('+home: HomeComponent', () => {
+      e2e.it('should have title', async () => {
+        const page = browser.goto(`${baseUrl}/`);
+
+        const text = await page
+          .evaluate(() => document.title)
+          .end();
+
+        e2e.e(text).toContain('showcasing the ng-seed app');
+      });
+    });
+  });
+});
