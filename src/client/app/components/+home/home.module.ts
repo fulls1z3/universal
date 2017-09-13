2ef23d78b178cb7123ee3b4b1cae57838e24270c
Burak Tasci <burak.tasci@cozumtech.net>
2017-09-13 21:33:27 +0300

    feat: use Angular Material components

--- src/client/app/components/+home/home.module.ts
+++ src/client/app/components/+home/home.module.ts
@@ -2,10 +2,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MdCardModule } from '@angular/material';
 
 // libs
 // TODO: ngx-i18n-router
 // import { I18NRouterModule } from '@ngx-i18n-router/core';
+
+// framework
+import { SharedModule } from '../../framework/core/shared.module';
 
 // routes & components
 import { routes } from './home.routes';
@@ -16,7 +20,9 @@ import { HomeComponent } from './home.component';
     CommonModule,
     // TODO: ngx-i18n-router
     // I18NRouterModule.forChild(routes, 'home')
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MdCardModule,
+    SharedModule
   ],
   declarations: [HomeComponent]
 })
--- src/client/app/framework/core/shared.module.ts
+++ src/client/app/framework/core/shared.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// libs
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   exports: [
     CommonModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    FlexLayoutModule,
+    TranslateModule
   ]
 })
 export class SharedModule {
