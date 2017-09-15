a1bb3ea3785d4b28151e00ac65b2696cc00307f7
Burak Tasci <me@fulls1z3.com>
2017-09-15 04:42:42 +0300

    test: fix broken e2e test (#123)

--- src/client/app/components/+secure/secure.component.e2e-spec.ts
+++ src/client/app/components/+secure/secure.component.e2e-spec.ts
@@ -5,14 +5,15 @@ import { e2e } from '../../framework/testing/e2e';
 e2e.describe('ng-seed/universal', () => {
   e2e.describe('components', () => {
     e2e.describe('+secure: SecureComponent', () => {
-      e2e.it('should have title', async () => {
+      e2e.it('should redirect to login page', async () => {
         const page = browser.goto(`${baseUrl}/secure-page`);
 
         const text = await page
           .evaluate(() => document.title)
           .end();
 
-        e2e.e(text).toContain('Secure page | ng-seed/universal');
+        // NOTE: should display the login page's title
+        e2e.e(text).toContain('Log in | ng-seed/universal');
       });
     });
   });
