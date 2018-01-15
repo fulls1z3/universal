736b2544d60c26f19677108fc3eda24223c6d702
Burak Tasci <me@fulls1z3.com>
2018-01-15 23:50:27 +0300

    Refactor general (#288)
    * fix: fix perfect-scrollbar directive
    
    * refactor: make use of rxjs imports

--- src/client/app/framework/i18n/src/language.effects.ts
+++ src/client/app/framework/i18n/src/language.effects.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 
 // libs
-import 'rxjs/add/operator/switchMap';
+import { map, switchMap } from 'rxjs/operators';
 import { Actions, Effect, toPayload } from '@ngrx/effects';
 
 // module
@@ -13,24 +13,28 @@ import * as language from './language.actions';
 @Injectable()
 export class LanguageEffects {
   @Effect() init$ = this.actions.ofType(language.INIT)
-    .map(toPayload)
-    .switchMap((settings: any) => this.i18n.init(settings))
-    .map((res: Language) => new language.UseLanguageSuccess(res));
+    .pipe(
+      map(toPayload),
+      switchMap((settings: any) => this.i18n.init(settings)),
+      map((res: Language) => new language.UseLanguageSuccess(res))
+    );
 
   @Effect() useLanguage$ = this.actions.ofType(language.USE_LANGUAGE)
-    .map(toPayload)
-    .switchMap((languageCode: string) => this.i18n.getLanguageByCode(languageCode))
-    .map((res: any) => {
-      if (res.name) {
-        const useLanguageSuccess = new language.UseLanguageSuccess(res);
+    .pipe(
+      map(toPayload),
+      switchMap((languageCode: string) => this.i18n.getLanguageByCode(languageCode)),
+      map((res: any) => {
+        if (res.name) {
+          const useLanguageSuccess = new language.UseLanguageSuccess(res);
 
-        // track analytics
-        this.i18n.track(useLanguageSuccess.type, {label: useLanguageSuccess.payload.code});
+          // track analytics
+          this.i18n.track(useLanguageSuccess.type, {label: useLanguageSuccess.payload.code});
 
-        return useLanguageSuccess;
-      } else
-        return new language.UseLanguageUnsupported(res.code);
-    });
+          return useLanguageSuccess;
+        } else
+          return new language.UseLanguageUnsupported(res.code);
+      })
+    );
 
   constructor(private readonly actions: Actions,
               private readonly i18n: I18NService) {
