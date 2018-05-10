ca88e1e7ede488ba8cd714a46433273ff0f9a258
Burak Tasci <me@fulls1z3.com>
2018-05-10 19:22:13 +0300

    fix: update base component

--- src/client/app/framework/core/src/base.component.ts
+++ src/client/app/framework/core/src/base.component.ts
@@ -1,9 +1,12 @@
 // angular
-import { OnDestroy } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 // libs
 import { Subject } from 'rxjs';
 
+@Component({
+  template: ''
+})
 export class BaseComponent implements OnDestroy {
   protected ngUnsubscribe: Subject<void>;
 
